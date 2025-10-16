189b41b45b84a7937ef52c5f83d3d94c83f3bcff
zhangqw <zhangqingweizhang82@gamil.com>
2025-10-16 14:18:14 +0800

    项目初始化

---
+++ src/main/java/com/zqw/agent_app/AgentAppApplication.java
@@ -0,0 +1,13 @@
+package com.zqw.agent_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zqw/agent_app/AgentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zqw.agent_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
