e75e7dd8adcdb46d60f2556f75acbd824cec53d6
zhangqw <zhangqingweizhang82@gamil.com>
2025-10-16 15:33:40 +0800

    编写小应用测试

---
+++ src/test/java/com/zqw/agent_app/SpringAITest.java
@@ -0,0 +1,26 @@
+package com.zqw.agent_app;
+
+import com.zqw.agent_app.app.CodeMaster;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+@SpringBootTest
+public class SpringAITest {
+
+    @Resource
+    private CodeMaster codeMaster;
+
+    @Test
+    void test() {
+        String chatId = UUID.randomUUID().toString();
+
+        String message = "git如何修改user和email配置";
+        String answer = codeMaster.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+
+    }
+}
